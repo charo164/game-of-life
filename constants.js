a230c32496c3a76e363c83e9c96e184cb01bbb51
charo164 <jjgcoly164@gmail.com>
2023-11-18 05:56:57 +0000

    refactor: :recycle: optimize code and add doc

--- constants.js
+++ constants.js
@@ -1,9 +1,9 @@
-const CELL_DIMENSION = 20;
-const ROWS = 35;
-const COLUMNS = 65;
-const DARK_COLOR = getComputedStyle(document.documentElement).getPropertyValue('--dark-color');
-const LIGHT_COLOR = getComputedStyle(document.documentElement).getPropertyValue('--light-color');
-const MODELS = {
+export const CELL_DIMENSION = 20;
+export const ROWS = Math.floor(document.documentElement.getBoundingClientRect().height / CELL_DIMENSION) - 6;
+export const COLUMNS = Math.floor(document.documentElement.getBoundingClientRect().width / CELL_DIMENSION) - 5;
+export const DARK_COLOR = getComputedStyle(document.documentElement).getPropertyValue('--dark-color');
+export const LIGHT_COLOR = getComputedStyle(document.documentElement).getPropertyValue('--light-color');
+export const MODELS = {
   glider: [
     [0, 1, 0],
     [0, 0, 1],
--- main.js
+++ main.js
@@ -1,153 +1,207 @@
-/**
- * @type {number[][]}
- */
-let state = [...Array(ROWS)].map((e) => Array(COLUMNS).fill(0));
-/**
- * @type {number[][]}
- */
-let nextState = [...Array(ROWS)].map((e) => Array(COLUMNS).fill(0));
-/**
- * @type {number}
- */
-let timer;
-let playing = false;
-let aliveCell = 0;
-let deathCell = ROWS * COLUMNS;
-
-const play = document.getElementById('play');
-const clear = document.getElementById('clear');
-const speed = document.getElementById('speed');
-const model = document.getElementById('model');
-const modal = document.querySelector('.modal');
-const modalWrapper = document.querySelector('.modal__wrapper');
-
-next.disabled = true;
-
-modalWrapper.addEventListener('click', stopPropagation);
-modal.addEventListener('click', closeModal);
-next.addEventListener('click', () => nextGeneration());
-clear.addEventListener('click', () => clearBoard());
-play.addEventListener('click', onPlayClicked);
-model.addEventListener('change', onPatternChange);
-
-window.addEventListener('load', () => {
-  initInfosModal();
-  generatePatterOption();
-  scrollToMiddleOfPage();
-  renderPopulation();
-});
-
-function scrollToMiddleOfPage() {
-  window.scrollTo(50, document.body.scrollHeight / 4, { behavior: 'smooth' });
-}
-
-function renderPopulation() {
-  const death = document.querySelector('#death');
-  const alive = document.querySelector('#alive');
-
-  if (death && alive) {
-    death.innerHTML = deathCell;
-    alive.innerHTML = aliveCell;
-  }
-}
+import { ROWS, COLUMNS, CELL_DIMENSION, MODELS } from './constants.js';
 
 /**
+ * @fileoverview Main file of the game of life.
+ * @version 0.0.1
+ * @module main
+ * @author jjgcoly164@gmail.com
+ * Represents a game object.
+ * @typedef {Object} Game
+ * @property {number[][]} state - The current state of the game grid.
+ * @property {number[][]} nextState - The next state of the game grid.
+ * @property {number} timer - The timer used for the game loop.
+ * @property {boolean} playing - Indicates whether the game is currently playing or not.
+ * @property {number} aliveCell - The number of alive cells in the game grid.
+ * @property {number} deathCell - The number of dead cells in the game grid.
  *
- * @param {Event} e
- */
-function onPlayClicked(e) {
-  e.preventDefault();
-
-  const i = document.querySelector('#play i');
-  const tooltip = document.querySelector('#play .tooltiptext');
-
-  if (playing) {
-    i?.classList.remove('fa-pause');
-    i?.classList.add('fa-play');
-    next.disabled = false;
-    clear.disabled = false;
-    if (tooltip) tooltip.innerHTML = 'Play';
-
-    clearInterval(timer);
-    playing = false;
-  } else {
-    i?.classList.remove('fa-play');
-    i?.classList.add('fa-pause');
-    next.disabled = true;
-    clear.disabled = true;
-    if (tooltip) tooltip.innerHTML = 'Pause';
-    timer = setInterval(
-      () => nextGeneration(1),
-      Number(speed.max) - Number(speed.value) + Number(speed.min),
-    );
-    playing = true;
-  }
-}
-
-/**
+ * @property {HTMLElement} play - The play button element.
+ * @property {HTMLElement} clear - The clear button element.
+ * @property {HTMLElement} speed - The speed input element.
+ * @property {HTMLElement} model - The model select element.
+ * @property {HTMLElement} modal - The modal element.
+ * @property {HTMLElement} next - The next button element.
+ * @property {HTMLElement} modalWrapper - The modal wrapper element.
+ * @property {HTMLElement} board - The game board element.
  *
- * @param {Event} e
+ * @property {function} initializeGrid - Initializes the game grid.
+ * @property {function} initState - Initializes the game state and next state.
+ * @property {function} init - Initializes the game.
+ * @property {function} renderPopulation - Renders the population statistics.
+ * @property {function} onPlayClicked - Handles the play button click event.
+ * @property {function} togglePlayButton - Toggles the play button icon and tooltip.
+ * @property {function} calculateSpeed - Calculates the game speed based on the speed input value.
+ * @property {function} onPatternChange - Handles the pattern change event.
+ * @property {function} initInfosModal - Initializes the information modal.
+ * @property {function} generatePatternOption - Generates the pattern options for the model select element.
+ * @property {function} createOptionElement - Creates an option element for the model select element.
+ * @property {function} createBoard - Creates the game board.
+ * @property {function} createCell - Creates a cell element for the game board.
+ * @property {function} toggle - Toggles the state of a cell.
+ * @property {function} nextGeneration - Advances the game to the next generation.
+ * @property {function} clearBoard - Clears the game board.
+ * @property {function} getHtmlCell - Retrieves the HTML element of a cell.
+ * @property {function} closeModal - Closes the modal.
+ * @property {function} openModal - Opens the modal.
+ * @property {function} stopPropagation - Stops the event propagation.
  */
-function onPatternChange(e) {
-  aliveCell = 0;
-  deathCell = ROWS * COLUMNS;
-  clearBoard();
-  scrollToMiddleOfPage();
-  const selected = e.target.value;
-  const model = MODELS[selected];
+export const game = {
+  state: [],
+  nextState: [],
+  timer: null,
+  playing: false,
+  aliveCell: 0,
+  deathCell: ROWS * COLUMNS,
+  play: document.getElementById('play'),
+  clear: document.getElementById('clear'),
+  speed: document.getElementById('speed'),
+  model: document.getElementById('model'),
+  modal: document.querySelector('.modal'),
+  next: document.getElementById('next'),
+  infos: document.getElementById('infos'),
+  skip: document.getElementById('skip'),
+  modalWrapper: document.querySelector('.modal__wrapper'),
+  board: document.querySelector('#board'),
 
-  if (model) {
-    const row = Math.floor(ROWS / 2) - Math.floor(model.length / 2);
-    const column = Math.floor(COLUMNS / 2) - Math.floor(model[0].length / 2);
+  initializeGrid() {
+    return Array.from({ length: ROWS }, () => Array(COLUMNS).fill(0));
+  },
 
-    for (let i = 0; i < model.length; ++i) {
-      for (let j = 0; j < model[0].length; ++j) {
-        state[row + i][column + j] = model[i][j];
-        if (model[i][j] === 1) {
-          getHtmlCell(row + i, column + j).classList.remove('dead');
-          aliveCell++;
-          deathCell--;
-        } else {
-          getHtmlCell(row + i, column + j).classList.add('dead');
+  initState() {
+    this.state = this.initializeGrid();
+    this.nextState = this.initializeGrid();
+  },
+
+  init() {
+    this.initState();
+    this.modalWrapper.addEventListener('click', this.stopPropagation.bind(this));
+    this.modal.addEventListener('click', this.closeModal.bind(this));
+    this.infos.addEventListener('click', this.openModal.bind(this));
+    this.skip.addEventListener('click', () => {
+      this.closeModal();
+      localStorage.setItem('showInfos', 'true');
+    });
+    this.play.addEventListener('click', this.onPlayClicked.bind(this));
+    this.clear.addEventListener('click', this.clearBoard.bind(this));
+    this.model.addEventListener('change', this.onPatternChange.bind(this));
+    this.next.addEventListener('click', () => this.nextGeneration(0));
+
+    window.addEventListener('load', () => {
+      this.initInfosModal();
+      this.generatePatternOption();
+      this.renderPopulation();
+    });
+
+    this.createBoard();
+  },
+
+  renderPopulation() {
+    const death = document.querySelector('#death');
+    const alive = document.querySelector('#alive');
+
+    if (death && alive) {
+      death.innerHTML = this.deathCell;
+      alive.innerHTML = this.aliveCell;
+    }
+  },
+
+  onPlayClicked(e) {
+    e.preventDefault();
+
+    if (this.playing) {
+      this.togglePlayButton('fa-pause', 'fa-play', 'Play');
+      clearInterval(this.timer);
+      this.playing = false;
+    } else {
+      this.togglePlayButton('fa-play', 'fa-pause', 'Pause');
+      this.timer = setInterval(() => this.nextGeneration(1), this.calculateSpeed());
+      this.playing = true;
+    }
+
+    this.clear.disabled = this.playing;
+    this.next.disabled = this.playing;
+  },
+
+  togglePlayButton(removeClass, addClass, tooltipText) {
+    const i = document.querySelector('#play i');
+    const tooltip = document.querySelector('#play .tooltipText');
+
+    i?.classList.remove(removeClass);
+    i?.classList.add(addClass);
+    if (tooltip) tooltip.innerHTML = tooltipText;
+  },
+
+  calculateSpeed() {
+    return Number(this.speed.max) - Number(this.speed.value) + Number(this.speed.min);
+  },
+
+  onPatternChange(e) {
+    this.aliveCell = 0;
+    this.deathCell = ROWS * COLUMNS;
+    this.clearBoard();
+    const selected = e.target.value;
+    const selectedModel = MODELS[selected];
+
+    if (selectedModel) {
+      const row = Math.floor(ROWS / 2) - Math.floor(selectedModel.length / 2);
+      const column = Math.floor(COLUMNS / 2) - Math.floor(selectedModel[0].length / 2);
+
+      for (let i = 0; i < selectedModel.length; ++i) {
+        for (let j = 0; j < selectedModel[0].length; ++j) {
+          this.state[row + i][column + j] = selectedModel[i][j];
+
+          const htmlCell = this.getHtmlCell(row + i, column + j);
+          if (selectedModel[i][j] === 1) {
+            htmlCell.classList.remove('dead');
+            this.aliveCell++;
+            this.deathCell--;
+          } else {
+            htmlCell.classList.add('dead');
+          }
         }
       }
+      this.renderPopulation();
     }
-    renderPopulation();
-  }
-}
+  },
 
-function initInfosModal() {
-  const showInfos = localStorage.getItem('showInfos');
+  initInfosModal() {
+    const showInfos = localStorage.getItem('showInfos');
 
-  if (showInfos === null) {
-    openModal();
-    localStorage.setItem('showInfos', 'false');
-  }
-}
+    if (showInfos === null) {
+      this.openModal();
+      localStorage.setItem('showInfos', 'false');
+    }
+  },
 
-function generatePatterOption() {
-  const defaultOption = document.createElement('option');
-  defaultOption.value = '';
-  defaultOption.innerHTML = 'Example de pattern';
-  const selectModel = document.getElementById('model');
-  selectModel.appendChild(defaultOption);
-  Object.keys(MODELS).map((model) => {
+  generatePatternOption() {
+    const defaultOption = this.createOptionElement('', 'Example de pattern');
+    this.model.appendChild(defaultOption);
+
+    Object.entries(MODELS).forEach(([modelName, model]) => {
+      const option = this.createOptionElement(modelName, modelName.split('_').join(' '));
+      this.model.appendChild(option);
+    });
+  },
+
+  createOptionElement(value, innerHTML) {
     const option = document.createElement('option');
-    option.value = model;
-    option.innerHTML = model.split('_').join(' ');
-    selectModel.appendChild(option);
-  });
-}
+    option.value = value;
+    option.innerHTML = innerHTML;
+    return option;
+  },
 
-function createBoard() {
-  for (let i = 0; i < ROWS; ++i) {
-    const row = document.createElement('tr');
-    for (let j = 0; j < COLUMNS; ++j) row.appendChild(createCell(i, j));
-    board.appendChild(row);
-  }
+  createBoard() {
+    this.board.style.gridTemplateColumns = `repeat(${COLUMNS}, ${CELL_DIMENSION}px)`;
+    this.board.style.gridTemplateRows = `repeat(${ROWS}, ${CELL_DIMENSION}px)`;
 
-  function createCell(i, j) {
-    const cell = document.createElement('td');
+    for (let i = 0; i < ROWS; ++i) {
+      for (let j = 0; j < COLUMNS; ++j) {
+        this.board.appendChild(this.createCell(i, j));
+      }
+    }
+  },
+
+  createCell(i, j) {
+    const cell = document.createElement('div');
     const nucleus = document.createElement('div');
     nucleus.classList.add('nucleus');
 
@@ -155,135 +209,104 @@ function createBoard() {
     cell.classList.add('cell', 'dead');
     cell.id = `${i} ${j}`;
 
-    cell.style.height = `${CELL_DIMENSION}px`;
-    cell.style.width = `${CELL_DIMENSION}px`;
-
-    cell.onmousedown = () => toggle(i, j);
-    cell.onmouseenter = (ev) => ev.buttons == 1 && toggle(i, j);
+    cell.onmousedown = () => this.toggle(i, j);
+    cell.onmouseenter = (ev) => ev.buttons === 1 && this.toggle(i, j);
 
     return cell;
-  }
-}
+  },
 
-/**
- * For filling the board with alive cells.
- * or for killing cells.
- *
- * @param {number} i
- * @param {number} j
- */
-function toggle(i, j) {
-  if (state[i][j] === 0) {
-    state[i][j] = 1;
-    getHtmlCell(i, j).classList.remove('dead');
-    aliveCell++;
-    deathCell--;
-  } else {
-    state[i][j] = 0;
-    getHtmlCell(i, j).classList.add('dead');
-    aliveCell--;
-    deathCell++;
-  }
+  toggle(i, j) {
+    if (this.state[i][j] === 0) {
+      this.state[i][j] = 1;
+      this.getHtmlCell(i, j).classList.remove('dead');
+      this.aliveCell++;
+      this.deathCell--;
+    } else {
+      this.state[i][j] = 0;
+      this.getHtmlCell(i, j).classList.add('dead');
+      this.aliveCell--;
+      this.deathCell++;
+    }
 
-  next.disabled = false;
-  renderPopulation();
-}
+    this.renderPopulation();
+  },
 
-/**
- * Calculates the next generation of the game of life based on the current state.
- * @param {number} [repeat=0] - If 1, sets a timer to repeat the calculation.
- */
-function nextGeneration(repeat = 0) {
-  aliveCell = 0;
-  deathCell = ROWS * COLUMNS;
-  for (let i = 0; i < ROWS; ++i) {
-    for (let j = 0; j < COLUMNS; ++j) {
-      const cell = state[i][j];
-      let liveNeighbors = 0;
+  nextGeneration(repeat = 0) {
+    this.aliveCell = 0;
+    this.deathCell = ROWS * COLUMNS;
 
-      for (let x = -1; x <= 1; ++x) {
-        for (let y = -1; y <= 1; ++y) {
-          if (i + x > 0 && j + y > 0 && i + x < ROWS && j + y < COLUMNS) {
-            liveNeighbors += state[i + x][j + y];
+    for (let i = 0; i < ROWS; ++i) {
+      for (let j = 0; j < COLUMNS; ++j) {
+        const cell = this.state[i][j];
+        let liveNeighbors = 0;
+
+        for (let x = -1; x <= 1; ++x) {
+          for (let y = -1; y <= 1; ++y) {
+            if (i + x >= 0 && j + y >= 0 && i + x < ROWS && j + y < COLUMNS) {
+              liveNeighbors += this.state[i + x][j + y];
+            }
           }
         }
-      }
 
-      liveNeighbors -= cell;
+        liveNeighbors -= cell;
 
-      if (cell === 1 && liveNeighbors < 2) nextState[i][j] = 0;
-      else if (cell === 1 && liveNeighbors > 3) nextState[i][j] = 0;
-      else if (cell === 0 && liveNeighbors === 3) nextState[i][j] = 1;
-      else nextState[i][j] = cell;
-    }
-  }
-
-  // render state
-  for (let i = 0; i < ROWS; ++i) {
-    for (let j = 0; j < COLUMNS; j++) {
-      if (nextState[i][j] === 1) {
-        getHtmlCell(i, j).classList.remove('dead');
-        aliveCell++;
-        deathCell--;
-      } else {
-        getHtmlCell(i, j).classList.add('dead');
+        if (cell === 1 && liveNeighbors < 2) this.nextState[i][j] = 0;
+        else if (cell === 1 && liveNeighbors > 3) this.nextState[i][j] = 0;
+        else if (cell === 0 && liveNeighbors === 3) this.nextState[i][j] = 1;
+        else this.nextState[i][j] = cell;
       }
     }
-  }
 
-  state = nextState;
-  nextState = [...Array(ROWS)].map((e) => Array(COLUMNS).fill(0));
-
-  renderPopulation();
-
-  if (repeat === 1) {
-    clearInterval(timer);
-    timer = setInterval(
-      () => nextGeneration(1),
-      Number(speed.max) - Number(speed.value) + Number(speed.min),
-    );
-  }
-}
-
-function clearBoard() {
-  for (let i = 0; i < ROWS; ++i) {
-    for (let j = 0; j < COLUMNS; j++) {
-      state[i][j] = 0;
-      getHtmlCell(i, j).classList.add('dead');
+    for (let i = 0; i < ROWS; ++i) {
+      for (let j = 0; j < COLUMNS; j++) {
+        const htmlCell = this.getHtmlCell(i, j);
+        if (this.nextState[i][j] === 1) {
+          htmlCell.classList.remove('dead');
+          this.aliveCell++;
+          this.deathCell--;
+        } else {
+          htmlCell.classList.add('dead');
+        }
+      }
     }
-  }
 
-  aliveCell = 0;
-  deathCell = ROWS * COLUMNS;
-  renderPopulation();
+    [this.state, this.nextState] = [this.nextState, this.initializeGrid()];
+    this.renderPopulation();
 
-  next.disabled = true;
-}
+    if (repeat === 1) {
+      clearInterval(this.timer);
+      this.timer = setInterval(() => this.nextGeneration(1), this.calculateSpeed());
+    }
+  },
 
-/**
- * Returns the html element of a cell.
- *
- * @param {number} i
- * @param {number} j
- */
-function getHtmlCell(i, j) {
-  return document.getElementById(`${i} ${j}`);
-}
+  clearBoard() {
+    for (let i = 0; i < ROWS; ++i) {
+      for (let j = 0; j < COLUMNS; j++) {
+        this.state[i][j] = 0;
+        this.getHtmlCell(i, j).classList.add('dead');
+      }
+    }
 
-function closeModal() {
-  modal.classList.remove('active');
-}
+    this.aliveCell = 0;
+    this.deathCell = ROWS * COLUMNS;
+    this.renderPopulation();
+  },
 
-function openModal() {
-  modal.classList.add('active');
-}
+  getHtmlCell(i, j) {
+    return document.getElementById(`${i} ${j}`);
+  },
 
-/**
- *
- * @param {Event} e
- */
-function stopPropagation(e) {
-  e.stopPropagation();
-}
+  closeModal() {
+    this.modal.classList.remove('active');
+  },
 
-createBoard();
+  openModal() {
+    this.modal.classList.add('active');
+  },
+
+  stopPropagation(e) {
+    e.stopPropagation();
+  },
+};
+
+game.init();
